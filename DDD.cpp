f31e65f0e8be105f467b0a7f6e954b99d23255cb
hian128 <128652922+hian128@users.noreply.github.com>
2024-04-10 21:05:14 -0300

    DDD.cpp

---
+++ DDD.cpp
@@ -0,0 +1,29 @@
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+int n;
+cout<<"digite o DDD"<<endl;
+cin>>n;
+if(n==61)
+cout<<"Brasilia";
+else if (n==71)
+cout<<"salvador";
+else if (n==11)
+cout<<"Sao paulo";
+else if (n==21)
+cout<<"rio de janeiro";
+else if (n==32)
+cout<<"juiz de fora";
+else if(n==19)
+cout<<"campinas";
+else if (n==27)
+cout<<"vitoria";
+else if (n==31)
+cout<<"belo horizonte";
+else 
+cout<<"DDD nao cadastrado";
+    return 0;
+}
